3adce87f0349cafffacb49bac2f30a269f43345d
Ricardo Huiscaleo llafquen <ricardohuiscaleollafquen@iMac-de-Ricardo.local>
2025-06-19 17:08:51 -0400

    Fix Netlify adapter configuration - build successful

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import netlify from '@astrojs/netlify';
+import netlify from '@astrojs/netlify/functions';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: netlify(),
+  adapter: netlify({}),
   experimental: {
     session: true,
   },
